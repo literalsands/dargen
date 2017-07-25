4fe4a9b9ee4f59fa83c79b42f91ed7d1d4a1d888
Austin New <literalsands@gmail.com>
2017-07-24 23:53:25 -0400

    added tictactoe example that doesn't quite work.

--- example-tictactoe.js
+++ example-tictactoe.js
@@ -1,41 +1,139 @@
 import { Population } from "./src/Population";
+import { Phenotype } from "./src/Phenotype";
+import { Genome } from "./src/Genome";
 
 let population = new Population({
   phenotype: {
     mutate: [
       {
-        name: g => (g > 0.5 ? "increment" : "decrement"),
+        name: "substitution",
         selection: {
-          rate: 0.4
-        },
-        params: {
-          increment: 0.03,
-          decrement: 0.03
+          rate: 0.05,
+          selection: "mutate.1.name"
         }
       },
       {
-        name: g => (g > 0.5 ? "duplication" : "removal"),
+        name: g => (g > 0.45 ? "duplication" : "removal"),
         selection: {
-          rate: 0.1
+          rate: 0.4,
+          selection: "player"
         }
       },
       {
         name: "substitution",
-        selection: 0.01
+        selection: {
+          rate: 0.1,
+          selection: "mutate.3.name"
+        }
+      },
+      {
+        name: g => (g > 0.5 ? "increment" : "decrement"),
+        selection: {
+          rate: 0.4,
+          selection: "player"
+        },
+        params: {
+          increment: g => 1 / 9 / 10 * g * 2,
+          decrement: g => 1 / 9 / 10 * g * 2
+        }
+      },
+      {
+        name: "substitution",
+        selection: {
+          rate: 0.1,
+          selection: "mutate.5.name"
+        }
+      },
+      {
+        name: g => (g > 0.5 ? "increment" : "decrement"),
+        selection: {
+          rate: 0.4,
+          selection: "player"
+        },
+        params: {
+          increment: g => 1 / 10 * g * 2,
+          decrement: g => 1 / 10 * g * 2
+        }
       }
     ],
-    player: function() {
+    player: function(a0, a1, a2, a3, a4, a5, a6, a7, a8) {
       // Convert the player epigenome into a neural network.
       // The first layer takes 9 values, the state of the board.
       // There are three states for the board. I guess I could set them as -1, 0, and 1.
-    },
-    ticTacToePlayer: function() {}
+      let inputs = 10,
+        outputs = 9,
+        network = Array.from(arguments).map(value => ({
+          input: Math.floor(value * inputs),
+          output: Math.floor(value * inputs % 1 * outputs),
+          weight: Math.cos(Math.PI * (value * 2 * inputs * outputs - 1) + 1) / 2
+        }));
+      function TicTacToePlayer(piece, board) {
+        let inputLayer = new Array(inputs).fill(0);
+        inputLayer.splice(0, 1, piece);
+        inputLayer.splice(1, 9, ...board);
+        // Apply network transformation.
+        let outputLayer = network.reduce((layer, { input, output, weight }) => {
+          let value = inputLayer[input] * weight;
+          // Set the output layer to the max input value.
+          if (value > layer[output]) layer[output] = value;
+          return layer;
+        }, new Array(outputs).fill(0));
+        let [position] = outputLayer.reduce(
+          ([choiceIndex, choiceValue], value, index) =>
+            // If there is no previous choice and this is a valid move.
+            choiceIndex === undefined && board[index] === 0
+              ? [index, value]
+              : // If there is a previous choice
+                choiceIndex !== undefined &&
+                // this is a valid move
+                board[index] === 0 &&
+                // and the value is higher.
+                value > choiceValue
+                ? [index, value]
+                : [choiceIndex, choiceValue],
+          [undefined, undefined]
+        );
+        board[position] = piece;
+      }
+      //console.log(Array.from(arguments))
+      return TicTacToePlayer;
+    }
   },
-  size: 50
+  size: 20
 });
 
+function RandomTicTacToePlayer(piece, board) {
+  // Use the randomly generated value of the genome to make copying the code easy.
+  let [position] = new Array(board.length).fill(0).map(Math.random).reduce((
+    [choiceIndex, choiceValue],
+    value,
+    index
+  ) =>
+    // If there is no previous choice and this is a valid move.
+    choiceIndex === undefined && board[index] === 0
+      ? [index, value]
+      : // If there is a previous choice
+        choiceIndex !== undefined &&
+        // this is a valid move
+        board[index] === 0 &&
+        // and the value is higher.
+        value > choiceValue
+        ? [index, value]
+        : [choiceIndex, choiceValue], [undefined, undefined]);
+  board[position] = piece;
+}
+
 function TicTacToe(player1, player2) {
+  // Convert the board into a string.
+  let drawBoard = board => {
+    let xoboard = board.map(space => (space ? (space > 0 ? "X" : "O") : " "));
+    return [xoboard.slice(0, 3), xoboard.slice(3, 6), xoboard.slice(6, 9)].join(
+      "\n"
+    );
+  };
+  // Nine squares.
   let board = new Array(9).fill(0);
+  // Three in a row!
   let wins = [
     // Horizontal
     [0, 1, 2],
@@ -49,6 +147,7 @@ function TicTacToe(player1, player2) {
     [0, 4, 8],
     [3, 4, 6]
   ];
+  // One of the "wins" is occupied by the same values 1 or -1.
   let winner = board => {
     return wins.filter(
       ([a, b, c]) =>
@@ -57,26 +156,60 @@ function TicTacToe(player1, player2) {
         board[a] === board[c]
     );
   };
-  let complete = () => winner(board) || board.every(p => p != 0);
+  let complete = () => winner(board).length > 0 || board.every(p => p !== 0);
+  // Player1 starts first.
+  let turn = player1;
+  // Stop if the game has concluded.
+  while (!complete()) {
+    // X's are represented by 1, O's are represented by -1.
+    let xo = turn === player1 ? 1 : -1;
+    // Player is given their piece to place, and the board.
+    let position = turn(xo, board);
+    // Alternate turns.
+    turn = turn === player1 ? player2 : player1;
+  }
+  //console.log("-------");
+  //console.log(drawBoard(board));
+  //console.log("-------");
+  let winning = winner(board);
+  return (winning.length && winning[0][0]) || 0;
 }
 
-for (let i = 0; i < 25000; i++) {
+for (let i = 0; i < 2000; i++) {
+  console.log("Generation", i)
   population.evolve({
     removal: individuals => {
-      console.log(
-        individuals[0].traits.valueA(),
-        "_____",
-        individuals[0].traits.valueB()
-      );
-      return individuals.slice(0, 4);
+      return individuals.slice(0);
     },
     survival: individuals => {
       return individuals.slice(0, 1);
     },
     groups: 5,
     fitness: (individual, group) => {
-      var game = TicTacToe.bind(null, individual.traits.player);
-      return group.map(opponent => game(opponent.traits.player));
+      let results = group
+        .map(opponent =>
+          TicTacToe(opponent.traits.player, individual.traits.player)
+        )
+        .concat(
+          new Array(10)
+            .fill(0)
+            .map(() =>
+              TicTacToe(individual.traits.player, RandomTicTacToePlayer)
+            )
+        )
+        .concat(
+          new Array(10)
+            .fill(0)
+            .map(() =>
+              TicTacToe(RandomTicTacToePlayer, individual.traits.player)
+            )
+        )
+        .filter(result => result <= 0).length;
+      if (20 + group.length - results < 2) {
+        console.log(individual.epigenome.selection("player").map((i) => individual.genome[i]))
+        console.log(20 + group.length - results, group.length);
+      }
+      return 20 + group.length - results;
     }
   });
 }
--- example.js
+++ example.js
@@ -1,67 +1,52 @@
 import { Population } from "./src/Population";
 
+let target = "It works!"
 let population = new Population({
   phenotype: {
     mutate: [
       {
-        name: g => (g > 0.5) ? "duplication" : "removal",
-        selection: {
-          rate: 0.1,
-          selection: "valueB"
-        },
-      },
-      {
-        name: g => (g > 0.5) ? "duplication" : "removal",
-        selection: {
-          rate: 0.1,
-          selection: "valueA"
-        },
-      },
-      {
         name: "substitution",
-        selection: 0.1
+        selection: {
+          selection: "value",
+          rate: 0.1
+        }
       }
     ],
-    slice: function(a) {
-      return Math.floor(a * 10);
-    },
-    valueA: function() {
+    value: function(a, b, c, d, e, f, g, h, i) {
       // I should be able to access other traits here.
-      var codes = Array.from(this.genome.slice(6,this.traits.slice)).map((arg) => {
+      var codes = Array.from(
+        arguments
+      ).map(arg => {
         return 32 + Math.floor(arg * (126 - 32));
       });
       var string = String.fromCharCode(...codes);
-      return string
+      return string;
     },
-    valueB: function() {
-      var codes = Array.from(this.genome.slice(this.traits.slice)).map((arg) => {
-        return 32 + Math.floor(arg * (126 - 32));
-      });
-      var string = String.fromCharCode(...codes);
-      return string
-    }
   },
-  size: 50
+  size: 5
 });
 
-for (let i = 0; i < 25000; i++) {
+for (let i = 0; i < 2000; i++) {
   population.evolve({
     removal: individuals => {
-      console.log(individuals[0].traits.valueA(), "_____", individuals[0].traits.valueB())
+      console.log(individuals[0].traits.value)
       return individuals.slice(0, 4);
     },
     survival: individuals => {
-      return individuals.slice(0, 1)
+      return individuals.slice(0, 1);
     },
     groups: 5,
+    // Ascending order.
+    comparison: ({value: a}, {value: b}) => {
+      return a - b;
+    },
+    // Add 1 to an error term for every mismatched letter.
     fitness: individual => {
-      let targetA = "It works!";
-      let valueA = individual.traits.valueA();
-      let retB = Array.from(targetA).reduce((n, v, i) => (targetA[i] === valueA[i] ? n + 1 : n), 0) / Math.abs(targetA.length - valueA.length)
-      let targetB = "You know it!";
-      let valueB = individual.traits.valueB();
-      let retA = Array.from(targetB).reduce((n, v, i) => (targetB[i] === valueB[i] ? n + 1 : n), 0) / Math.abs(targetB.length - valueB.length)
-      return retA + retB;
+      let value = individual.traits.value;
+      let ret = Array.from(target).reduce(
+          (n, v, i) => (target[i] === value[i] ? n : n + 1),
+          0)
+      return ret;
     }
   });
 }
