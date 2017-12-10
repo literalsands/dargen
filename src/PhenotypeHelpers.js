7ccb44e0e6cf07a0dcf7b2fd4b9ece344cccbd49
Austin New <literalsands@gmail.com>
2017-12-09 23:20:23 -0800

    export phenotype helper functions individually

--- src/PhenotypeHelpers.js
+++ src/PhenotypeHelpers.js
@@ -1,39 +1,36 @@
 /**
  * @module PhenotypeHelpers
  */
-const PhenotypeHelpers = {
-  /**
-   * Chooses an element from an array.
-   * Use with 0 to 1 genomes.
-   *
-   * @function choose
-   * @memberof PhenotypeHelpers
-   * @param {Array} choices - Array to make choice from.
-   * @returns {Function}
-   */
-  choose: function(arr) {
-    if (!Array.isArray(arr)) throw TypeError("choose takes argument array");
-    return function(unit) {
-      return unit === 1
-        ? arr[arr.length - 1]
-        : arr[Math.floor(unit * arr.length)];
-    };
-  },
-  /**
-   * Sets a desired 'length' for the function.
-   * This is just the starting size if using duplication and/or removal.
-   *
-   * @function arity
-   * @memberof PhenotypeHelpers
-   * @param {integer} arity - Desired length or arity of the function.
-   * @param {Function} func - Function to set the length on.
-   * @returns {Function}
-   */
-  arity: function(length, func) {
-    if (!(typeof length === "number" && func instanceof Function)) throw TypeError("arity takes arguments number and function")
-    Object.defineProperty(func, "length", {value: length});
-    return func;
-  }
-};
-
-export default PhenotypeHelpers;
+/**
+ * Chooses an element from an array.
+ * Use with 0 to 1 genomes.
+ *
+ * @function choose
+ * @namespace PhenotypeHelpers
+ * @param {Array} choices - Array to make choice from.
+ * @returns {Function}
+ */
+export function choose(arr) {
+  if (!Array.isArray(arr)) throw TypeError("choose takes argument array");
+  return function(unit) {
+    return unit === 1
+      ? arr[arr.length - 1]
+      : arr[Math.floor(unit * arr.length)];
+  };
+}
+/**
+ * Sets a desired 'length' for the function.
+ * This is just the starting size if using duplication and/or removal.
+ *
+ * @function arity
+ * @namespace PhenotypeHelpers
+ * @param {integer} arity - Desired length or arity of the function.
+ * @param {Function} func - Function to set the length on.
+ * @returns {Function}
+ */
+export function arity(length, func) {
+  if (!(typeof length === "number" && func instanceof Function))
+    throw TypeError("arity takes arguments number and function");
+  Object.defineProperty(func, "length", { value: length });
+  return func;
+}
